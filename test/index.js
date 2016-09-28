e3ac26d7db63120616da22e4605d3a2e84f35175
Scott Warren <scott_warren@flightcentre.com>
2016-09-28 19:39:32 +1000

    adding base tests

--- test/index.js
+++ test/index.js
@@ -2,7 +2,9 @@ import assert from 'assert';
 import passwordJs from '../lib';
 
 describe('password-js', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+  it('should return a fixed length of password!', function () {
+    assert(passwordJs.generatePassword({
+      passwordLength: 10
+    }).length, 10);
   });
 });
