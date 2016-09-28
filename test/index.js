6f17be198183c5dc18f4d72b1ee739f86ea8b719
Scott Warren <scott_warren@flightcentre.com>
2016-09-28 21:11:35 +1000

    Adds tests and adds 'generate' password method :)

--- test/index.js
+++ test/index.js
@@ -1,10 +1,25 @@
 import assert from 'assert';
-import passwordJs from '../lib';
+import Password from '../lib';
+
+let passwordGenerator;
 
 describe('password-js', function () {
+  beforeEach(function(done) {
+    passwordGenerator = new Password({
+      passwordLength: 15
+    });
+
+    done();
+  })
+
+
   it('should return a fixed length of password!', function () {
-    assert(passwordJs.generatePassword({
-      passwordLength: 10
-    }).length, 10);
+    passwordGenerator.setPasswordLength(10);
+
+    assert(passwordGenerator.generate().length, 10);
+  });
+
+  it('should generate a password that does NOT include symbols that are not allowed', function() {
+    assert(passwordGenerator.generate().indexOf("'") === -1, true)
   });
 });
